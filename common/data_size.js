a18c42779a2e7ac9595fa5a394727803a2999c08
Anthony <deflexable@gmail.com>
2025-10-17 22:53:57 +0100

    removed radio button web component

--- common/data_size.js
+++
@@ -1,3 +0,0 @@
-export const one_kb = 1024,
-    one_mb = one_kb * 1024,
-    one_gb = one_mb * 1024;
--- web/components/PopupModal/PopupModal.d.ts
+++ web/components/PopupModal/PopupModal.d.ts
@@ -1,5 +1,3 @@
 import { Popup } from "reactjs-popup";
 
 export default Popup;
-
-export const popupModalStyle: [{}];
